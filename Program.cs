66df6fb1d475c395cea33a78a69a24f84922c6ec
Abdur Rehman <73142548+rehmani999@users.noreply.github.com>
2022-09-18 22:21:45 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // --------------------- Week 1 Lab 1-----------------------------------------
+            //int num;
+            //Console.WriteLine("Enter a number:");
+            //num = Convert.ToInt32(Console.ReadLine());
+            //int secNum;
+            //Console.WriteLine("Enter Second Number:");
+            //secNum = Convert.ToInt32(Console.ReadLine());
+
+            //int sum = num + secNum;
+            //Console.WriteLine("The sum of {0} and {1} is: {2}", num, secNum, sum);
+
+            ////Console.WriteLine("You enter the number is : " + num);
+            ////Console.WriteLine("You enter number {0} and {1}", num, secNum);
+            //Console.ReadLine();
+
+            // -------------------------- Week 1 Task 2 --------------------------------
+
+            //int num;
+            //Console.Write("Enter a number:");
+            //num = Convert.ToInt32(Console.ReadLine());
+
+            //if(num %2 == 0)
+            //{
+            //    Console.Write("Number is even");
+            //}
+            //else
+            //{
+            //    Console.Write("Number is Odd");
+            //}
+            //Console.ReadLine();
+
+            //----------------------- Task 3 ---------------------------
+
+            //float num1;
+            //Console.WriteLine("Enter a number: ");
+            //num1 = Convert.ToSingle(Console.ReadLine());
+            //float num2;
+            //Console.WriteLine("Enter a number: ");
+            //num2 = Convert.ToSingle(Console.ReadLine());
+
+            //float num3 = Convert.ToSingle(num1 * num2);
+
+            //Console.WriteLine("result is:{0}", num3);
+            //Console.ReadKey();
+
+            //--------------------------- Task 4 ------------------------------------
+
+            //string name;
+            //Console.Write("Enter any country name: ");
+            //name = Console.ReadLine();
+
+            //Console.WriteLine(name.Length);
+            //Console.ReadLine();
+
+            // --------------------------- Task 5 --------------------------------------
+            //string sen = "I live in Pakistan";
+            //string[] convertSen = sen.Split(' ');
+            //Console.WriteLine(convertSen.Length);
+            //Console.ReadLine();
+
+            //----------------------------------Task 6 -------------------------------
+            //Console.WriteLine("Please Enter Your Prefrence press 1 for Dollars  to Repee and Press 2 for rupee to dollars:");
+            //int num = Convert.ToInt32(Console.ReadLine());
+            //if(num == 1)
+            //{
+            //    int num2;
+            //    Console.WriteLine("Please Enter amount in dollars:");
+            //    num2 = Convert.ToInt32(Console.ReadLine());
+            //    int res1 = Convert.ToInt32(num2 * 240);
+            //    Console.WriteLine("The Result is {0}", res1);
+            //}
+            //else
+            //{
+            //    int num3;
+            //    Console.WriteLine("Please Enter amount in dollars:");
+            //    num3 = Convert.ToInt32(Console.ReadLine());
+            //    int res2 = Convert.ToInt32(num3 /260);
+            //    Console.WriteLine("The Result is {0}", res2);
+            //}
+
+            //Console.ReadLine();
+
+            //-------------------------------- Task 7 --------------------------------------
+            
+
+        }
+    }
+}
